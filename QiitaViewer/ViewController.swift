888d93e17a9f457c0e4e122e11c12ac1f5a61f7d
ユーザー１ <user1@yuza1-no-macbook-pro.local>
2018-11-27 14:58:04 +0900

    Initial Commit

---
+++ QiitaViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QiitaViewer
+//
+//  Created by ユーザー１ on 2018/11/27.
+//  Copyright © 2018 Masaya Ujihara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
